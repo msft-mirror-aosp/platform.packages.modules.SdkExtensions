b1ffe651c4e1c07ebab46abc4ebc0488cf32d423
Anton Hansson <hansson@google.com>
2021-04-27 10:09:48 +0100

    Change extension array to map of all extensions
    Instead of returning a mutable array that could conceivably cause
    problems at some point, return an unmodifiable map containing all
    extensions and their versions.
    
    Bug: 185567297
    Test: cts + e2e tests in this CL
    Change-Id: I11fb4448782dd0c8b84d96f89544f1ca0747078f

--- tests/cts/src/android/os/ext/cts/SdkExtensionsTest.java
+++ tests/cts/src/android/os/ext/cts/SdkExtensionsTest.java
@@ -21,6 +21,7 @@ import android.os.SystemProperties;
 import android.os.ext.SdkExtensions;
 import com.android.modules.utils.build.SdkLevel;
 import junit.framework.TestCase;
+import java.util.Map;
 
 public class SdkExtensionsTest extends TestCase {
 
@@ -51,11 +52,13 @@ public class SdkExtensionsTest extends TestCase {
     }
 
     public void testExtensionVersions() throws Exception {
+        Map<Integer, Integer> versions = SdkExtensions.getAllExtensionVersions();
         int expectedSize = 1;
         if (SdkLevel.isAtLeastS()) {
             expectedSize++;
         }
-        assertEquals(expectedSize, SdkExtensions.EXTENSIONS.length);
+        assertEquals(0, (int) versions.get(Build.VERSION_CODES.R));
+        assertEquals(expectedSize, versions.size());
     }
 
 }
